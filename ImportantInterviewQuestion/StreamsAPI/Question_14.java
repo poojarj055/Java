c3420363dc0eb096059cc393526b02dd5a65b8ba
PoojaRaj <poojarj055@gmail.com>
2024-12-24 20:20:51 +0530

    Update Question_14.java

--- ImportantInterviewQuestion/StreamsAPI/Question_14.java
+++ ImportantInterviewQuestion/StreamsAPI/Question_14.java
@@ -1,2 +1,20 @@
 // Given a list of words, create a map of word frequencies using streams.
 
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> str = Arrays.asList("ad", "am", "did", "ad", "am", "dad");
+        
+       // Use streams to compute the word frequencies
+        Map<String, Long> wordFrequencies = str.stream()
+            .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
+
+        System.out.println(wordFrequencies);
+       
+    }
+    
+}
+
