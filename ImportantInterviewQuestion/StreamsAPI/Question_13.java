3485643f202b667230a7cc364fc78c6a0f1ac111
PoojaRaj <poojarj055@gmail.com>
2024-12-24 19:22:08 +0530

    Update Question_13.java

--- ImportantInterviewQuestion/StreamsAPI/Question_13.java
+++ ImportantInterviewQuestion/StreamsAPI/Question_13.java
@@ -6,11 +6,40 @@ import java.util.stream.*;
 public class Main {
     public static void main(String[] args) {
         List<String> str = Arrays.asList("vgyihbj", "ah", "did", "ad", "tyy", "vnbvnb");
+
+        // Use mapToInt and average to calculate average length
+        double avgLength = str.stream()
+                              .mapToInt(String::length)
+                              .average()
+                              .orElse(0.0); // Default value if the list is empty
+
+        System.out.println(avgLength);
+    }
+}
+
+
+------------ABOVE one is recommended-----------------------------
+
+
+import java.util.*;
+import java.util.stream.*;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> str = Arrays.asList("vgyihbj", "ah", "did", "ad", "tyy", "vnbvnb");
         
         int sumLength=str.stream().map(s-> s.length()).reduce(0, (a,b)-> a+b);
-        double avgLength=sumLength/str.size();
+        double avgLength=(double)sumLength/str.size();
         System.out.println(avgLength);
        
     }
     
 }
+
+
+
+
+
+
+
+
