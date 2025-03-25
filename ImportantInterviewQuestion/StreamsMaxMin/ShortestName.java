b573071b12766fa1a4ec72fa616467e7fa5f6ab7
PoojaRaj <poojarj055@gmail.com>
2025-03-25 10:26:58 +0530

    Create ShortestName.java

---
+++ ImportantInterviewQuestion/StreamsMaxMin/ShortestName.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Problem:Given a list of student names, find the student with the shortest name.
+        List<String> students = Arrays.asList("Amit", "Jonathan", "Sam", "Elizabeth");
+        System.out.println(students.stream().min(Comparator.comparingDouble(String::length)).orElse(null));
+        }
+}
