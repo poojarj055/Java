7350fa74e87174729ef87d30ee65b5c12dca4d7d
PoojaRaj <poojarj055@gmail.com>
2025-03-30 18:43:47 +0530

    Create WordWithPrefix.java

---
+++ ImportantInterviewQuestion/WordWithPrefix.java
@@ -0,0 +1,25 @@
+You are given an array of strings words and a string pref.
+Return the number of strings in words that contain pref as a prefix.
+A prefix of a string s is any leading contiguous substring of s.
+
+Example 1:
+
+Input: words = ["pay","attention","practice","attend"], pref = "at"
+Output: 2
+Explanation: The 2 strings that contain "at" as a prefix are: "attention" and "attend".
+Example 2:
+
+Input: words = ["leetcode","win","loops","success"], pref = "code"
+Output: 0
+Explanation: There are no strings that contain "code" as a prefix.
+
+
+
+class Solution {
+    public int prefixCount(String[] words, String pref) {
+         return (int) Arrays.stream(words)
+                .filter(word -> word.startsWith(pref)) 
+                .count();
+    }
+}
+  
