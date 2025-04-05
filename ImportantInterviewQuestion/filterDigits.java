27ca19fe052c55db4790fa6692efe9a9bd7a12a4
PoojaRaj <poojarj055@gmail.com>
2025-04-05 11:18:17 +0530

    Update filterDigits.java

--- ImportantInterviewQuestion/filterDigits.java
+++ ImportantInterviewQuestion/filterDigits.java
@@ -9,3 +9,18 @@ public class Main {
             .forEach(System.out::println);     // prints: 12, 43, 50
     }
 }
+
+
+////another way
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> list = List.of("ABC", "DEF", "12", "43", "50", "GHI");
+
+        list.stream()
+            .filter(str -> str.chars().allMatch(Character::isDigit)) // checks if all chars are digits
+            .forEach(System.out::println); // prints: 12, 43, 50
+    }
+}
